b3ae14b56f740e94ea71aee787148e9c1f0926a1
Zahid Rafique <zrafique@lib.usc.edu>
2023-09-12 19:37:38 -0700

    Implemented on 9/5/2023 & 9/12/2023

--- PriorityQ.java
+++ PriorityQ.java
@@ -2,36 +2,61 @@
 
 public class PriorityQ {
     //  Todo: Declare the array to store the queue data
-
+    long data[];
+    
     //  Todo: Declare the variables to maintain the queue
-
+    int numValidValues;
+    
     //  Todo: Implement the constructor
     public PriorityQ(int maxSize) {
-        
+        data = new long[maxSize];
+        numValidValues = 0;
     }
 
     //  Todo: insert the passed value in the queue
-    public void insert(long item) {        
-
+    public void insert(long item) { 
+        if (isFull()) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        int i;
+        for(i=numValidValues-1; i >= 0; i--) {
+            if (data[i] < item) {
+                data[i+1] = data[i];
+            } else {
+                break;
+            }
+        }
+        
+        data[i+1] = item;
+        numValidValues++;
     }
 
     //  Todo: Remove the minimum item
     public long remove() {
-        return -1;
+        if (numValidValues == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+                
+        return data[--numValidValues];
     }
 
     //  Todo: return the minimum item without removing
     public long peekMin() {
-        return -1;
+        if (numValidValues == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        return data[numValidValues - 1];
     }
 
     // Todo: Implement
     public boolean isEmpty() {
-        return false;
+        return numValidValues == 0;
     }
 
     // Todo: Implement
     public boolean isFull() {
-        return false;
+        return numValidValues == data.length;
     }
 }
--- Queue.java
+++ Queue.java
@@ -2,43 +2,68 @@
 
 public class Queue {
     //  Todo: Declare the array to store data for queue
-    
+    long[] data;
     //  Todo: Declare the variables to maintain the queue
-
+    int front;
+    int back;
+    int numValidValues;
+    
     //  Todo: Impelment the constructor
     public Queue(int maxSize) {
-
+        data = new long[maxSize];
+        front = 0;
+        back = -1;
+        numValidValues = 0;
     }
 
     //  Todo: put item at rear of queue
     public void insert(long j) {
-
+        if (isFull()) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        if (back == data.length - 1) {
+            back = -1;
+        }
+        
+        data[++back] = j;
+        numValidValues++;
     }
 
     //  Todo: take item from front of queue
     public long remove() {
-
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        long temp = data[front];
+        front++;
+        numValidValues--;
+        
+        if (!isEmpty() && front == data.length) {
+            front = 0;
+        }
         //  Todo: Throw an exception since 0 could be a valid value
-        return 0;       
+        return temp;       
     }
 
     //  Todo: return item from front of queue without removing
     public long peekFront() {
-        return -1;
+        return data[front - 1];
     }
 
     //  Todo: Implement
     public boolean isEmpty() {
-        return false;
+        return numValidValues == 0;
     }
 
     //  Todo: Implement
     public boolean isFull() {
-        return false;
+        return numValidValues == data.length;
     }
 
     //  Todo: Implement
     public int size() {
-        return 0;
+        return numValidValues;
     }
 }
